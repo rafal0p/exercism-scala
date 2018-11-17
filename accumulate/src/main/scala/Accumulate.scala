8b621f43acf47dfa08beaa7f988b229b7fce7b30
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:32:34 +0100

    accumulate iteration 2

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -1,7 +1,13 @@
+import scala.annotation.tailrec
+
 class Accumulate {
-  def accumulate[A, B](f: A => B, list : List[A]): List[B] =
+  def accumulate[A, B](f: A => B, list: List[A]): List[B] =
+    _accumulate(f, list, List[B]())
+
+  @tailrec
+  private def _accumulate[A, B](f: A => B, list: List[A], result: List[B]): List[B] =
     list match {
-      case head :: tail => f(head) :: accumulate(f, tail)
-      case _ => List[B]()
+      case head :: tail => _accumulate(f, tail, result :+ f(head))
+      case _ => result
     }
 }
