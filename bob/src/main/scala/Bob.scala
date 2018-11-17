edeedabd9a0f289b4b537248751998caa46deea0
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:19:41 +0100

    bob iteration 3

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -2,21 +2,43 @@ object Bob {
 
   def response(statement: String): String =
     statement match {
-      case _ if isShout(statement) && isQuestion(statement) => onShoutedQuestion
-      case _ if isShout(statement) => onShout
-      case _ if isQuestion(statement) => onQuestion
-      case _ if isSilence(statement) => onSilence
+      case isShoutedQuestion(res) => res
+      case isShout(res) => res
+      case isSilence(res) => res
+      case isQuestion(res) => res
       case _ => onAnythingElse
     }
 
-  private def isShout(statement: String) =
+  object isSilence {
+    def unapply(str: String): Option[String] = ifIs(silence(str), onSilence)
+  }
+
+  object isQuestion {
+    def unapply(str: String): Option[String] = ifIs(question(str), onQuestion)
+  }
+
+  object isShout {
+    def unapply(str: String): Option[String] = ifIs(shout(str), onShout)
+  }
+
+  object isShoutedQuestion {
+    def unapply(str: String): Option[String] = ifIs(shout(str) && question(str), onShoutedQuestion)
+  }
+
+  private def shout(statement: String) =
     statement.exists(_.isLetter) && statement.equals(statement.toUpperCase())
 
-  private def isQuestion(statement: String) =
+  private def question(statement: String) =
     statement.trim().endsWith("?")
 
-  private def isSilence(statement: String) =
-    statement.trim() == ""
+  private def silence(str: String) = str.trim() == ""
+
+  private def ifIs(condition: Boolean, some: String): Option[String] = {
+    if (condition)
+      Some(some)
+    else
+      None
+  }
 
   private val onShout = "Whoa, chill out!"
   private val onQuestion = "Sure."
