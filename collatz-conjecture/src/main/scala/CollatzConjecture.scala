9e823ce91906fdbb50f1da5457b89ee08af0007f
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:13:48 +0100

    collatz-conjecture iteration 3

--- collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,22 +1,11 @@
 import scala.annotation.tailrec
 
 object CollatzConjecture {
-  def steps(i: Int): Option[Int] = {
-    val (total, _) = countSteps(0, i)
-    total
-  }
-
   @tailrec
-  private def countSteps(total: Int, current: Int): (Option[Int], Int) =
-    current match {
-      case i if i <= 0 => (None, i)
-      case 1 => (Some(total), 1)
-      case _ => countSteps(total + 1, collatz(current))
-    }
-
-  private def collatz(n: Int) =
-    n match {
-      case _ if n % 2 == 0 => n / 2
-      case _ => 3 * n + 1
-    }
+  def steps(n: Int, count: Int = 0): Option[Int] = n match {
+    case 1 => Some(count)
+    case x if x <= 0 => None
+    case x if x % 2 == 0 => steps(x / 2, count + 1)
+    case x => steps(3 * x + 1, count + 1)
+  }
 }
