a89e9c01f6ef35212805eecd900442c385ceae53
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:14:05 +0100

    collatz-conjecture iteration 4

--- collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,11 +1,15 @@
-import scala.annotation.tailrec
-
 object CollatzConjecture {
-  @tailrec
-  def steps(n: Int, count: Int = 0): Option[Int] = n match {
-    case 1 => Some(count)
-    case x if x <= 0 => None
-    case x if x % 2 == 0 => steps(x / 2, count + 1)
-    case x => steps(3 * x + 1, count + 1)
+
+  def steps(i: Int): Option[Int] = {
+    if (i <= 0)
+      None
+    else
+      Some(Stream.iterate(i)(collatz).takeWhile(_ != 1).length)
   }
+
+  private def collatz(n: Int) =
+    n match {
+      case _ if n % 2 == 0 => n / 2
+      case _ => 3 * n + 1
+    }
 }
