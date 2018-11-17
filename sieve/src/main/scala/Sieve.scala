e9a878a43302b55f655c83479284dd9ecc313283
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:04:02 +0100

    sieve solution 5

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -4,16 +4,14 @@ object Sieve {
     findPrimes(List(), (2 to max).toList)
   }
 
-  private def findPrimes(foundSoFar: List[Int], list: List[Int]): List[Int] = {
-    if (list.nonEmpty) {
-      val head = list.head
-      val tail = list.tail
-      findPrimes(head :: foundSoFar, withoutMultiples(of = head, from = tail))
-    } else {
-      foundSoFar.reverse
-    }
+  private def findPrimes(foundSoFar: List[Int],
+                         list: List[Int]): List[Int] = list match {
+    case head :: tail =>
+      findPrimes(head :: foundSoFar, tail.filter(multiples(of = head)))
+    case _ => foundSoFar.reverse
   }
 
-  private def withoutMultiples(of: Int, from: List[Int]) =
-    from.filter(i => i % of != 0)
+  private def multiples(of: Int) = {
+    i: Int => i % of != 0
+  }
 }
