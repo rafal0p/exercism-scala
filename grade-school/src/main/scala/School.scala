798772cb3fa5a5fe2ecf65ea95e320d961f03c30
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:29:16 +0100

    grade-school iteration 6

--- grade-school/src/main/scala/School.scala
+++ grade-school/src/main/scala/School.scala
@@ -3,15 +3,15 @@ import scala.collection.immutable.SortedMap
 class School {
   type DB = Map[Int, Seq[String]]
 
-  private var state = SortedMap[Int, Seq[String]]()
+  private var state = SortedMap[Int, Seq[String]]().withDefaultValue(Seq())
 
   def add(name: String, g: Int): Unit = {
-    state += (g -> (grade(g) :+ name))
+    state += (g -> (state(g) :+ name))
   }
 
   def db: DB = state
 
-  def grade(g: Int): Seq[String] = state.getOrElse(g, Seq())
+  def grade(g: Int): Seq[String] = state(g)
 
   def sorted: DB = state.mapValues(_.sorted)
 }
