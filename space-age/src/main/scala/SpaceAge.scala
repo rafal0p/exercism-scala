1860e416f3159d5829d190cc1fd6441f3cbf502c
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:24:22 +0100

    space-age iteration 4

--- space-age/src/main/scala/SpaceAge.scala
+++ space-age/src/main/scala/SpaceAge.scala
@@ -1,22 +1,25 @@
 object SpaceAge {
-  val onEarth: Double => Double = ageInYearsOnPlanetWith()
+  val onEarth = AgeInYearsOnPlanetWith()
 
-  val onMercury: Double => Double = ageInYearsOnPlanetWith(0.2408467)
+  val onMercury = AgeInYearsOnPlanetWith(orbitalPeriod = 0.2408467)
 
-  val onVenus: Double => Double = ageInYearsOnPlanetWith(0.61519726)
+  val onVenus = AgeInYearsOnPlanetWith(orbitalPeriod = 0.61519726)
 
-  val onMars: Double => Double = ageInYearsOnPlanetWith(1.8808158)
+  val onMars = AgeInYearsOnPlanetWith(orbitalPeriod = .8808158)
 
-  val onJupiter: Double => Double = ageInYearsOnPlanetWith(11.862615)
+  val onJupiter = AgeInYearsOnPlanetWith(orbitalPeriod = .862615)
 
-  val onSaturn: Double => Double = ageInYearsOnPlanetWith(29.447498)
+  val onSaturn = AgeInYearsOnPlanetWith(orbitalPeriod = .447498)
 
-  val onUranus: Double => Double = ageInYearsOnPlanetWith(84.016846)
+  val onUranus = AgeInYearsOnPlanetWith(orbitalPeriod = .016846)
 
-  val onNeptune: Double => Double = ageInYearsOnPlanetWith(164.79132)
+  val onNeptune = AgeInYearsOnPlanetWith(orbitalPeriod = .79132)
 
-  private def ageInYearsOnPlanetWith(orbitalPeriod: Double = 1)(ageInSeconds: Double) =
-    ageInSeconds / yearInSecondsOnPlanetWith(orbitalPeriod)
+  case class AgeInYearsOnPlanetWith(orbitalPeriod: Double = 1) {
+    def apply(ageInSeconds: Double): Double = {
+      ageInSeconds / yearInSecondsOnPlanetWith(orbitalPeriod)
+    }
+  }
 
   private def yearInSecondsOnPlanetWith(orbitalPeriod: Double) = orbitalPeriod * earthYearInSeconds
 
