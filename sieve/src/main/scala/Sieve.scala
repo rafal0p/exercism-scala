c2f9eff2bce81d01df9bd877c82f1a5fa1bcab28
Rafal Podlipny <rafal0p@outlook.com>
2018-11-17 09:02:22 +0100

    sieve solution 2

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -1,17 +1,29 @@
 object Sieve {
   def primes(max: Int): List[Int] = {
-    var primes = List[Int]()
-    var notPrimes = Set[Int]()
+    var numbers = (2 to max).map(i => (i, Option.empty[Boolean]))
 
-    def isNotYetExcluded(n: Int) = !notPrimes.contains(n)
-
-    for (potentialPrime <- 2 to max) {
-      if (isNotYetExcluded(potentialPrime)) {
-        primes = primes ++ List(potentialPrime)
-        notPrimes = notPrimes ++ multiplesOf(potentialPrime, max)
+    def markMultiplesOf(number: Int) = {
+      numbers.map {
+        case (i, _) if i == number => (i, Option(true))
+        case (i, _) if multiplesOf(number, max).contains(i) => (i, Option(false))
+        case (i, isEmpty) => (i, isEmpty)
       }
     }
-    primes
+
+    var n = findFirstUncheckedIn(numbers)
+    while (n.nonEmpty) {
+      numbers = markMultiplesOf(n.get)
+      n = findFirstUncheckedIn(numbers)
+    }
+
+    numbers
+      .filter { case (_, isPrime) => isPrime.getOrElse(false) }
+      .map { case (i, _) => i }
+      .toList
+  }
+
+  private def findFirstUncheckedIn(numbers: IndexedSeq[(Int, Option[Boolean])]) = {
+    numbers find { case (_, isPrime) => isPrime.isEmpty } map { case (i, _) => i }
   }
 
   private def multiplesOf(n: Int, upTo: Int) = {
