2d0fed2eb57b5ecb30c23074a379bb6d9aaf76dc
Rafal Podlipny <rafal0p@outlook.com>
2018-11-18 14:39:43 +0100

    hamming iteration 6
    maybe crazy option handling

--- hamming/src/main/scala/Hamming.scala
+++ hamming/src/main/scala/Hamming.scala
@@ -1,13 +1,12 @@
 object Hamming {
   def distance(left: String, right: String): Option[Int] =
-    Option(left.length == right.length)
-      .filter(identity)
-      .map(_ =>
-        (left zip right)
-          .map {
-            case (l, r) if l == r => 0
-            case _ => 1
-          }
-          .sum
-      )
+    Some(() =>
+      (left zip right)
+        .map {
+          case (l, r) if l == r => 0
+          case _ => 1
+        }
+        .sum
+    ).filter(_ => left.length == right.length)()
+
 }
