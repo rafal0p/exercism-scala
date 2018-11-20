1ff8bc506ee85b64d38ae898fd8b88a07f8ee154
Rafal Podlipny <rafal0p@outlook.com>
2018-11-20 18:18:11 +0100

    phone-number iteration 2

--- phone-number/src/main/scala/PhoneNumber.scala
+++ phone-number/src/main/scala/PhoneNumber.scala
@@ -1,14 +1,26 @@
 object PhoneNumber {
-  def clean(dirty: String): Option[String] = {
-    val digits = dirty.filter(_.isDigit)
-    digits.toList match {
-      case '1' :: t if t.length == 10 => Some(t.mkString)
-      case '0' :: _ => None
-      case '1' :: _ => None
-      case _ :: _ :: _ :: '0' :: _ => None
-      case _ :: _ :: _ :: '1' :: _ => None
-      case _ :: t if t.length == 9 => Some(digits)
-      case _ => None
-    }
+  def clean(raw: String): Option[String] = {
+    val number = trimCountryCode(raw.filter(_.isDigit))
+    if (startsWithAnyOf(Set('0', '1'), areaCode(number))
+      || startsWithAnyOf(Set('0', '1'), localNumber(number)))
+      None
+    else
+      number
   }
+
+  private def trimCountryCode(digits: String) = {
+    if (digits.length == 11 && digits(0) == '1')
+      Some(digits.drop(1))
+    else if (digits.length == 10)
+      Some(digits)
+    else
+      None
+  }
+
+  private def localNumber(number: Option[String]) = number.map(_.drop(3))
+
+  private def areaCode(number: Option[String]) = number.map(_.take(3))
+
+  private def startsWithAnyOf(chars: Set[Char], string: Option[String]) =
+    string.map(_ (0)).exists(chars.contains)
 }
