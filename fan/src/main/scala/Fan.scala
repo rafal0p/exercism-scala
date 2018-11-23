e412cd567ccd29e2ef010d3211c4dd2c8925aafc
Rafal Podlipny <rafal0p@outlook.com>
2018-11-23 20:56:45 +0100

    fan iteration 2

--- fan/src/main/scala/Fan.scala
+++ fan/src/main/scala/Fan.scala
@@ -1,6 +1,8 @@
 object Fan {
   def generate(size: Int): String = {
     val bigSize = 2 * size - 1
+    val on = '*'
+    val off = '.'
 
     def shouldFill(x: Int, y: Int) = {
       if (x < size && y < size && y >= x)
@@ -15,11 +17,19 @@ object Fan {
         false
     }
 
+    def charToPrint(x: Int, y: Int) = {
+      if (shouldFill(x, y)) on else off
+    }
+
     plane(2 * size)
-      .map { case (x, y) => (x, y, if (shouldFill(x, y)) "*" else ".") }
+      .map { case (x, y) => (x, y, charToPrint(x, y)) }
       .groupBy { case (_, y, _) => y }
       .toSeq.sortBy { case (y, _) => -y }
-      .map { case (_, line) => line.map { case (_, _, c) => c }.mkString }
+      .map { case (_, line) => line
+        .sortBy { case (x, _, _) => x }
+        .map { case (_, _, c) => c }
+        .mkString
+      }
       .mkString("\n")
   }
 
