f8b711dc577e8e4d9c005aefd3dd193414aff8c2
Rafal Podlipny <rafal0p@outlook.com>
2018-11-25 18:16:11 +0100

    secret-handshake iteration 2
    hide tailrec in inner function

--- secret-handshake/src/main/scala/SecretHandshake.scala
+++ secret-handshake/src/main/scala/SecretHandshake.scala
@@ -1,15 +1,17 @@
 import scala.annotation.tailrec
 
 object SecretHandshake {
-  @tailrec
-  def commands(code: Int,
-               acc: List[String] = List(),
-               shouldReverse: Boolean = false): List[String] = code match {
-    case 0 => if (shouldReverse) acc.reverse else acc
-    case _ if code / 16 >= 1 => commands(code - 16, acc, true)
-    case _ if code / 8 >= 1 => commands(code - 8, "jump" :: acc, shouldReverse)
-    case _ if code / 4 >= 1 => commands(code - 4, "close your eyes" :: acc, shouldReverse)
-    case _ if code / 2 >= 1 => commands(code - 2, "double blink" :: acc, shouldReverse)
-    case 1 => commands(code - 1, "wink" :: acc, shouldReverse)
+  def commands(code: Int): List[String] = {
+    @tailrec
+    def _commands(code: Int, acc: List[String], shouldReverse: Boolean): List[String] = code match {
+      case 0 => if (shouldReverse) acc.reverse else acc
+      case _ if code / 16 >= 1 => _commands(code - 16, acc, true)
+      case _ if code / 8 >= 1 => _commands(code - 8, "jump" :: acc, shouldReverse)
+      case _ if code / 4 >= 1 => _commands(code - 4, "close your eyes" :: acc, shouldReverse)
+      case _ if code / 2 >= 1 => _commands(code - 2, "double blink" :: acc, shouldReverse)
+      case 1 => _commands(code - 1, "wink" :: acc, shouldReverse)
+    }
+
+    _commands(code, List(), shouldReverse = false)
   }
 }
