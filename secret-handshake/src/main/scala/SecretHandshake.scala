00cfece5f992f2aad7f28a22903282e6a13db634
Rafal Podlipny <rafal0p@outlook.com>
2018-11-28 09:14:09 +0100

    secret-handshake iteration 6
    express reverse in the same map as addition

--- secret-handshake/src/main/scala/SecretHandshake.scala
+++ secret-handshake/src/main/scala/SecretHandshake.scala
@@ -1,22 +1,18 @@
 object SecretHandshake {
   def commands(code: Int): List[String] =
-    if (isOn(bitPos = 4, in = code))
-      actionsIn(code).reverse
-    else
-      actionsIn(code)
+    actions
+      .filter { case (key, _) => isOn(bitPos = key, in = code) }
+      .values
+      .foldLeft(List[String]())((acc, apply) => apply(acc))
+      .reverse
 
   private def isOn(bitPos: Int, in: Int) = (in & (1 << bitPos)) != 0
 
-  private def actionsIn(code: Int) =
-    availableActions
-      .filter { case (key, _) => isOn(bitPos = key, in = code) }
-      .values
-      .toList
-
-  private val availableActions: Map[Int, String] = Map(
-    0 -> "wink",
-    1 -> "double blink",
-    2 -> "close your eyes",
-    3 -> "jump"
+  private val actions: Map[Int, List[String] => List[String]] = Map(
+    0 -> (list => "wink" :: list),
+    1 -> (list => "double blink" :: list),
+    2 -> (list => "close your eyes" :: list),
+    3 -> (list => "jump" :: list),
+    4 -> (list => list.reverse)
   )
 }
