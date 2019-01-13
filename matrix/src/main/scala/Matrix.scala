1084f4b7e8c113c9904dba61815b59e420ae8095
Rafal Podlipny <rafal0p@outlook.com>
2019-01-13 21:44:26 +0100

    matrix iteration 1

--- matrix/src/main/scala/Matrix.scala
+++ matrix/src/main/scala/Matrix.scala
@@ -1,5 +1,10 @@
-case class Matrix(st: String) {
-  def column(i: Int) = Vector()
+case class Matrix(raw: String) {
+  private val rawRows = raw.split('\n')
+  private val rows = rawRows.map(row => row.split(' ').map(_.toInt).toVector)
+  private val rowSize = rows.map(row => row.size).max
+  private val cols = (0 until rowSize).map(i => rows.map(row => row(i)).toVector)
 
-  def row(i: Int) = Vector()
+  def row(i: Int): Vector[Int] = rows(i)
+
+  def column(i: Int): Vector[Int] = cols(i)
 }
