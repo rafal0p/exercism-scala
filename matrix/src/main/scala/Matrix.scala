8e4b9e5606aa6104e3b2842da9818205246dc607
Rafal Podlipny <rafal0p@outlook.com>
2019-01-26 08:38:04 +0100

    matrix iteration 4
    there is a function StringLike#lines !

--- matrix/src/main/scala/Matrix.scala
+++ matrix/src/main/scala/Matrix.scala
@@ -1,9 +1,12 @@
-case class Matrix(raw: String) {
-  private lazy val rawRows = raw.split('\n').toVector
-  private lazy val rows = rawRows.map(row => row.split(' ').map(_.toInt).toVector)
+case class Matrix(input: String) {
+  private lazy val rows = input.lines.map(lineToInts).toVector
   private lazy val cols = rows.transpose
 
   def row(i: Int): Vector[Int] = rows(i)
 
   def column(i: Int): Vector[Int] = cols(i)
+
+  private def lineToInts(line: String) = {
+    line.split(' ').map(_.toInt).toVector
+  }
 }
